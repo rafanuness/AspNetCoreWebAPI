f81efea9c03e954974f7b91041253d68cc38c4ab
Rafael Santos <rafaeln2005749@hotmail.com>
2020-08-20 16:46:45 -0300

    Adding my course on GitHub

--- SmartSchoolAPI/Models/Professor.cs
+++ SmartSchoolAPI/Models/Professor.cs
@@ -6,15 +6,9 @@ namespace SmartSchoolAPI.Models
     {
         public Professor()
         {
-
+            
         }
 
-        public Professor(int id, string nome)
-        {
-            this.Id = id;
-            this.Nome = nome;
-
-        }
         public int Id { get; set; }
         public string Nome { get; set; }
         public IEnumerable<Disciplina> Disciplina { get; set; }
